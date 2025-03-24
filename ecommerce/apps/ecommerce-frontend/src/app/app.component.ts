77b881e13023811a02d7149cd385973b891c0fb5
Lucas Lima <lucaserolima@gmail.com>
2025-03-23 22:29:28 -0400

    fix(app): configure FontAwesome icons with correct prefix and add to library

--- ecommerce/apps/ecommerce-frontend/src/app/app.component.ts
+++ ecommerce/apps/ecommerce-frontend/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecommerce-frontend';
+export class AppComponent implements OnInit {
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
