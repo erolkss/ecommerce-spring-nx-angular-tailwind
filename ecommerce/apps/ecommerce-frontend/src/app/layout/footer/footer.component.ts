4fe7cca02bee4a18e2475fc50d3dca493ad58199
Lucas Lima <lucaserolima@gmail.com>
2025-03-24 22:50:34 -0400

    feat(layout): add footer and integrate into app component

---
+++ ecommerce/apps/ecommerce-frontend/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'ecom-footer',
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
