7573f1898bf8c095eb0de6ac7a625b2063672de5
Lucas Lima <lucaserolima@gmail.com>
2025-03-28 22:19:15 -0400

    feat: integrate dotenv for environment variable management

--- ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/EcommerceBackendApplication.java
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/EcommerceBackendApplication.java
@@ -1,13 +1,18 @@
 package br.com.ero.ecommerce_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    dotenv.entries().forEach(entry ->
+      System.setProperty(entry.getKey(), entry.getValue())
+    );
+    SpringApplication.run(EcommerceBackendApplication.class, args);
+  }
 
 }
