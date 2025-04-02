1bbec9c0c9a22b71245ef0683ef2ffe0ddea2eb8
Lucas Lima <lucaserolima@gmail.com>
2025-04-02 10:30:52 -0400

    feat(database): configure JPA, transactions, auditing, and web support

---
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/wire/postgresql/infrastructure/DataBaseConfiguration.java
@@ -0,0 +1,17 @@
+package br.com.ero.ecommerce_backend.wire.postgresql.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"br.com.ero.ecommerce_backend"})
+@EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class DataBaseConfiguration {
+
+
+}
