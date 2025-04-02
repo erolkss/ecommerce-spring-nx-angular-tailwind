31a71e7024aee88d1e5625bd98f3cea86beae018
Lucas Lima <lucaserolima@gmail.com>
2025-04-02 10:28:38 -0400

    feat(security): add JWT authentication converter

---
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/shared/authentication/infrastructure/primary/KindeAuthenticationConverter.java
@@ -0,0 +1,31 @@
+package br.com.ero.ecommerce_backend.shared.authentication.infrastructure.primary;
+
+import br.com.ero.ecommerce_backend.shared.authentication.application.AuthenticatedUser;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class KindeAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+  @Override
+  public AbstractAuthenticationToken convert(@NotNull Jwt source) {
+    return new JwtAuthenticationToken(source,
+      Stream.concat(new JwtGrantedAuthoritiesConverter().convert(source).stream(), extractResourceRoles(source).stream()).collect(Collectors.toSet()));
+  }
+
+  private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+    return AuthenticatedUser.extractRolesFromToken(jwt).stream()
+      .map(SimpleGrantedAuthority::new)
+      .collect(Collectors.toSet());
+  }
+
+}
