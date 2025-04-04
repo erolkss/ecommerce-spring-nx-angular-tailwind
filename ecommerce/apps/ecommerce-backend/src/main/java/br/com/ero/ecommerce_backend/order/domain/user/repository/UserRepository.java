9be723897d7ae204b5278a9db8649d69e0644fda
Lucas Lima <lucaserolima@gmail.com>
2025-04-03 23:22:59 -0400

    feat(user): add UserRepository interface with basic user operations

---
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/order/domain/user/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package br.com.ero.ecommerce_backend.order.domain.user.repository;
+
+import br.com.ero.ecommerce_backend.order.domain.user.aggregate.User;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserAddress;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserEmail;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserPublicId;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+  void save(User user);
+
+  Optional<User> get(UserPublicId userPublicId);
+
+  Optional<User> getOneByEmail(UserEmail userEmail);
+
+  void updateAddress(UserPublicId userPublicId, UserAddress userAddress);
+}
