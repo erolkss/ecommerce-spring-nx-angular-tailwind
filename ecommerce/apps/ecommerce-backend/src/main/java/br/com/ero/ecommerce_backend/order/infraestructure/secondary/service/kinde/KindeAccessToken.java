b33a86f0a9e21c7b18326b787c3e1b602cd3a2b2
Lucas Lima <lucaserolima@gmail.com>
2025-04-03 23:33:33 -0400

    feat(auth): add KindeAccessToken record for deserializing token response

---
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/order/infraestructure/secondary/service/kinde/KindeAccessToken.java
@@ -0,0 +1,7 @@
+package br.com.ero.ecommerce_backend.order.infraestructure.secondary.service.kinde;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KindeAccessToken(@JsonProperty("access_token") String accessToken,
+                               @JsonProperty("token_type") String tokenType) {
+}
