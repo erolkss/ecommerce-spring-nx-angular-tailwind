b4f557a759ad5bf3a826c4172c77f5e8118f2655
Lucas Lima <lucaserolima@gmail.com>
2025-04-07 15:43:13 -0400

    feat(repository): create SpringDataUserRepository and implement save method

---
+++ ecommerce/apps/ecommerce-backend/src/main/java/br/com/ero/ecommerce_backend/order/infraestructure/secondary/repository/SpringDataUserRepository.java
@@ -0,0 +1,51 @@
+package br.com.ero.ecommerce_backend.order.infraestructure.secondary.repository;
+
+import br.com.ero.ecommerce_backend.order.domain.user.aggregate.User;
+import br.com.ero.ecommerce_backend.order.domain.user.repository.UserRepository;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserAddressToUpdate;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserEmail;
+import br.com.ero.ecommerce_backend.order.domain.user.vo.UserPublicId;
+import br.com.ero.ecommerce_backend.order.infraestructure.secondary.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class SpringDataUserRepository implements UserRepository {
+
+  private final JpaUserRepository jpaUserRepository;
+
+  public SpringDataUserRepository(JpaUserRepository jpaUserRepository) {
+    this.jpaUserRepository = jpaUserRepository;
+  }
+
+
+  @Override
+  public void save(User user) {
+    if (user.getDbId() != null) {
+      Optional<UserEntity> userToUpdateOpt = jpaUserRepository.findById(user.getDbId());
+      if (userToUpdateOpt.isPresent()) {
+        UserEntity userToUpdate = userToUpdateOpt.get();
+        userToUpdate.updateFromUser(user);
+        jpaUserRepository.saveAndFlush(userToUpdate);
+      }
+    } else {
+      jpaUserRepository.save(UserEntity.from(user));
+    }
+  }
+
+  @Override
+  public Optional<User> get(UserPublicId userPublicId) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<User> getOneByEmail(UserEmail userEmail) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void updateAddress(UserPublicId userPublicId, UserAddressToUpdate userAddress) {
+
+  }
+}
