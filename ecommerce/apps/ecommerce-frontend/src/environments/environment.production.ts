0dccc5b53b7408853ad1bdba7e9a51ab823593c3
Lucas Lima <lucaserolima@gmail.com>
2025-04-11 17:03:18 -0400

    build(config): add environment configurations for production and development with file replacements

---
+++ ecommerce/apps/ecommerce-frontend/src/environments/environment.production.ts
@@ -0,0 +1,10 @@
+export const enviroment = {
+    kinde: {
+        authority: "https://erobr.kinde.com",
+        redirectUrl: "http://localhost:4200",
+        postLogoutRedirectUri: "http://localhost:4200",
+        clientId: "0cebc90375c54efab4dffe29682d3061",
+        audience: "https://erobr.kinde.com/api"
+    },
+    apiUrl: "http://localhost:8080/api"
+};
