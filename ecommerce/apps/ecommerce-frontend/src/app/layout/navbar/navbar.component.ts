f61dfacfa9a512aee0967c4c39c6edbb5182b7fe
Lucas Lima <lucaserolima@gmail.com>
2025-04-15 23:04:33 -0400

    feat(navbar): display user avatar if connected, fallback to icon otherwise

--- ecommerce/apps/ecommerce-frontend/src/app/layout/navbar/navbar.component.ts
+++ ecommerce/apps/ecommerce-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Oauth2Service } from '../../auth/oauth2.service';
 
 @Component({
   selector: 'ecom-navbar',
@@ -8,4 +9,34 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  oAuth2Service = inject(Oauth2Service);
+
+  connectedUserQuery = this.oAuth2Service.connectedUserQuery;
+
+
+  login() {
+    this.closeDropDownMenu();
+    this.oAuth2Service.login();
+  }
+
+  logout() {
+    this.closeDropDownMenu();
+    this.oAuth2Service.logout();
+  }
+
+  isConnected(): boolean {
+    return this.connectedUserQuery?.status() === 'success'
+      && this.connectedUserQuery?.data()?.email !== this.oAuth2Service.notConnected;
+  }
+
+  closeDropDownMenu() {
+    const bodyElement = document.activeElement as HTMLBodyElement;
+    if (bodyElement) {
+      bodyElement.blur();
+    }
+  }
+
+
+}
