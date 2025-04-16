53a5f66aa85a8d48533738f380d9ac7787f0060a
Lucas Lima <lucaserolima@gmail.com>
2025-04-15 23:05:05 -0400

    chore(env): update Kinde audience URL in environment config

--- ecommerce/apps/ecommerce-frontend/src/environments/environment.ts
+++ ecommerce/apps/ecommerce-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const enviroment = {
         redirectUrl: "http://localhost:4200",
         postLogoutRedirectUri: "http://localhost:4200",
         clientId: "0cebc90375c54efab4dffe29682d3061",
-        audience: "https://erobr.kinde.com/api"
+        audience: "http://localhost:8080/api"
     },
     apiUrl: "http://localhost:8080/api"
 };
